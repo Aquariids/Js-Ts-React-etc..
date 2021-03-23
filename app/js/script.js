3086377841393b33375a368b38a265e8a47aa938
Dima-kun <FavoritePonyville@gmail.com>
2021-03-23 12:39:08 +0400

    variables and use

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,18 @@
-const obj = {
-    a: 10
-};
-obj.a = 5;
-alert(obj.a);
+var x = 2;
+{
+
+    var x = 1;
+    
+}
+
+console.log(x); // будет 1 var перезаписала себя!
+
+let y = 2;
+{
+
+    let y = 1;
+    console.log(y); // будет 1
+    
+}
+
+console.log(y); // будет 2
