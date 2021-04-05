3acb76a11d05bbe8658e42fcfc412264825e4d11
Dima-kun <FavoritePonyville@gmail.com>
2021-04-05 12:19:18 +0400

    added operators

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log(150 % 50);
+function getRandomPers () {
+    
+     let gendere = Math.floor(Math.random() > 0.5);
+      if (gendere === 0) {
+
+      }
+    
+
+}
+
+console.log(getRandomPers());
