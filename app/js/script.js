a91de7833840adfddc6abf5466b36473e82cab7c
Dima-kun <favoriteponyville@gmail.com>
2021-04-06 17:40:01 +0400

    added info about git

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+
+
+const blocks = document.querySelectorAll('.block'),
+		  wrapper = document.querySelector('.wrapper');
+
+
+wrapper.addEventListener ('click', (event) => {
+	if (event.target && event.target.tagName == 'SECTION') {
+  	blocks[0].classList.add('red');
+  	blocks[0].classList.contains('red');
+  }
+})
+
+
