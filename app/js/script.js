34b188621e14e040330bec6eab5b6f5ff246cffc
Dima-kun <favoriteponyville@gmail.com>
2021-04-06 20:49:57 +0400

    deleted some little things

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,3 @@
 'use strict';
 
 
-
-const blocks = document.querySelectorAll('.block'),
-		  wrapper = document.querySelector('.wrapper');
-
-
-wrapper.addEventListener ('click', (event) => {
-	if (event.target && event.target.tagName == 'SECTION') {
-  	blocks[0].classList.add('red');
-  	blocks[0].classList.contains('red');
-  }
-})
-
-
