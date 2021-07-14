2cb5d2d44b6197959566562713905ea37a70b422
Ponyville <favoritePonyville@gmail.com>
2021-07-14 21:51:19 +0400

    EEE LEARNING

---
+++ app/all/Programming/basics of programming/cs50.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main(void) {
+int x = 10;
+int y = 10;
+if (x < y) {
+    printf("x is less than y");
+} else if(x > y) {
+    printf("x is greater than y\n");
+} else {
+    printf("is big than y and x\n");
+}
+
+}
