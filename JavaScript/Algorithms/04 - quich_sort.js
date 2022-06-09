3e84ce3ee632842ec269ace3b3938d01fe49267e
Dima-kun <FaboritePonyville@gmail>
2022-06-10 00:51:32 +0300

    docs(javaScript/Algorithms/selection_sort.md) add info)

--- JavaScript/Algorithms/04 - quich_sort.js
+++ JavaScript/Algorithms/04 - quich_sort.js
@@ -15,3 +15,29 @@ function fac (n) {
 
 console.log(fac(5)); 
 
+const arr = [9,2,3,5,9,1,-10,10,58]; // массив который нужно отсортировать.
+
+console.log(arr.length);
+let count = 0; // наше кол операций
+
+function selectionSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        
+        let indexMin = i;
+            for(let j = i+1; j < arr.length; j++) {
+                if(arr[j] < arr[indexMin]) {
+                    indexMin = j;
+                }
+                count += 1; // кол операций
+            }
+
+            let tmp = arr[i];
+            arr[i] = arr[indexMin];
+            arr[indexMin] = tmp;
+
+    }
+    
+    return arr;
+}
+console.log(selectionSort(arr));
+console.log(count); // 36 итераций на массив из 9 элементов.
